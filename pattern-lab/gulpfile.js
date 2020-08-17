cdf7f03dd611ce99b56911a563e44f6c243eb64b
indrajith <indrajith.koloorkavil@gmail.com>
2020-08-17 09:52:34 +0530

    Initial commit

---
+++ pattern-lab/gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var del = require('del');
+
+gulp.task('scss', function() {
+    return gulp.src('./source/_patterns/_patterns-global/style.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./source/css/')) 
+});
+
+gulp.task('clean', function() {
+    return del('./source/css/style.css');
+})
+
+gulp.task('watch', function(){
+    return gulp.watch('./source/**/*.scss', gulp.series(['clean', 'scss']));
+});
+
+gulp.task('default', gulp.series('watch'));
